5ea616001096bacacb803dfdd745890a0b22c7d5
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-01-25 13:46:03 +0100

    Add project files.

---
+++ MeyawoPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+using Microsoft.Ajax.Utilities;
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbMyPortfolioEntities db = new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult FuturePartial()
+        {
+            var values = db.TblFuture.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult AboutPartial()
+        {
+            var values = db.TblAbout.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult ServicePartial()
+        {
+            var values = db.TblService.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult ProjectPartial() {
+
+            var values = db.TblProject.ToList();
+            return PartialView(values);
+
+        }
+        public PartialViewResult TestmonialPartial()
+        {
+
+            var values = db.TblTestimonial.ToList();
+            return PartialView(values);
+
+        }
+
+
+
+    }
+}
---
+++ MeyawoPortfolio/Controllers/ProjectController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+using Microsoft.Ajax.Utilities;
+
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        DbMyPortfolioEntities db = new DbMyPortfolioEntities();
+         
+        public ActionResult Index()
+        {
+            var values = db.TblProject.ToList();
+            return View(values);
+        }
+        public ActionResult CreateProject()
+        {
+            return View();
+        }
+    }
+}
